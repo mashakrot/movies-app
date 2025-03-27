9fe7bf05222d60bd688cd823b7aa6d434d211e43
mashakrot <makrosensei@gmail.com>
2025-03-27 18:23:54 +0200

    01

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/movies-app",
+  build: {
+    outDir: "dist"
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
