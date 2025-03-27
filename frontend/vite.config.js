415a711e90feb44759e3d7d722fe149056cf78f3
mashakrot <makrosensei@gmail.com>
2025-03-27 19:06:12 +0200

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/movies-app",
+  // base: "/movies-app",
+  base: "https://mashakrot.github.io/movie-app/",
   build: {
     outDir: "dist"
   },
