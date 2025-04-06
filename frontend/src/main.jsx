70764e817b4684f0925f1bc7fdbd1c0897b33593
mashakrot <makrosensei@gmail.com>
2025-04-06 21:01:58 +0300

    new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/movies-app">
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
